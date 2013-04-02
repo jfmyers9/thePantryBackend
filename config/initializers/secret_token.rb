1e51fe5c27cdf0a7552808494cdfcc911630619c
James Myers <jfmyers9@gmail.com>
2013-04-01 21:02:09 -0700

    starting over

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ThePantryBackend::Application.config.secret_token = 'ada2d3e326fb750fa2a2edfbc82031ae130f79375f2b3c61b27bdc09932ed31445d5b4e2fa8eeccb1f9b5ac9d23a6520b1d4664c49fc58a09a94d2116bc0b274'
