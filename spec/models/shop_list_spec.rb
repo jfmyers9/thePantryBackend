92a422dc45aef03fe29f28c3fd42044cd631738c
Christina Hang <christinah0113@gmail.com>
2013-04-03 04:05:42 -0700

    add rspec shoplist and inventory

--- spec/models/shop_list_spec.rb
+++ spec/models/shop_list_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe ShopList do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @test = ShopList.new(ingredient: "carrots", user_id: 2)}
+
+  subject{ @test }
+  
+  it { should respond_to(:ingredient) }
+  it { should respond_to(:user_id) }
+  
+  it { should be_valid }
+  
+  describe "duplicate entry" do
+    @ingr = ShopList.new(ingredient: "carrots", user_id: 2)
+    expect { @ingr.save }.to not_change { ShopList.count }
+  end
+  
+  describe "no ingredient to add" do
+    before { @test.ingredient = " " }
+    it { should_not be_valid }
+    after { @test.ingredient = "carrots" }
+  end
+  
+  describe "no user id" do
+    before { @test.user_id = nil }
+    it { should_not be_valid }
+    after { @test.user_id = 2 }
+  end
 end
