dc3c29dfa08f13a80c0c07b9c72d9dceb830aa9e
James Myers <jfmyers9@gmail.com>
2013-05-02 21:08:49 -0700

    changes

--- app/models/user_recipe.rb
+++ app/models/user_recipe.rb
@@ -1,14 +1,14 @@
 class UserRecipe < ActiveRecord::Base
 
-  attr_accessible :cooked, :favorite, :recipe_id, :imageUrl, :ingLines, :name, :user_id, :dirLines
+  attr_accessible :cooked, :favorite, :recipe_id, :image_url, :ing_lines, :name, :user_id, :dir_lines
 
   belongs_to :user
   
   validates :recipe_id, :presence => true
   validates :user_id, :presence => true
   validates :name, :presence => true
-  validates :ingLines, :presence => true
-  validates :dirLines, :presence => true
+  validates :ing_lines, :presence => true
+  validates :dir_lines, :presence => true
 
   validates_uniqueness_of :name, :recipe_id => [:user_id]
 
