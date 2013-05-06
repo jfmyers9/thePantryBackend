bdc3c9bb592e8d1ea6c4abb2d8fcee13d74251b7
James Myers <jfmyers9@gmail.com>
2013-05-06 13:05:11 -0700

    searching by name allowed as well

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -41,7 +41,7 @@ class UserRecipeController < ApplicationController
     if params[:ingredients] != nil
       @recs = UserRecipe
       params[:ingredients].each do |ingredient|
-        @recs = @recs.where("ing_lines like ?", "%" + ingredient + "%")
+        @recs = @recs.where("ing_lines like ? OR name like ?", "%" + ingredient + "%", "%" + ingredient + "%")
       end
       @reclist = @recs.map do |rec|
         { :id => rec.recipe_id, :name => rec.name, :cooked => rec.cooked, :favorite => rec.favorite,
